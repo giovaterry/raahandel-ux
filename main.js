e36337c273588989724211af218a66b89ee63e06
giovaterry <74530756+giovaterry@users.noreply.github.com>
2021-04-26 12:49:22 +0200

    value item
    value

--- main.js
+++ main.js
@@ -16,3 +16,15 @@ function myFunction() {
       }
     }
   }
+
+let btnAdd = document.querySelector("#add");
+let btnSubtract = document.querySelector("#subtract");
+let input = document.querySelector("input");
+
+btnAdd.addEventListener("click", ()=>{
+  input.value = parseInt(input.value) +1;
+});
+
+btnSubtract.addEventListener("click", ()=>{
+  input.value = parseInt(input.value) -1;
+});
